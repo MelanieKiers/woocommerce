0236bb6001f960abbdb24fd30a1569d49195b0c0
Paul Sealock <psealock@gmail.com>
2021-12-02 16:47:55 +1300

    save

--- .github/actions/changelogger-pull-request/index.js
+++ .github/actions/changelogger-pull-request/index.js
@@ -6,7 +6,12 @@ const diff = core.getInput( 'diff' );
 const rxString = `.*(?=\/changelog\/${ branch }).*`;
 const rx = new RegExp( rxString, 'gm' );
 
-if ( rx.test( diff ) ) {
-	const [ changelogFilePath ] = rx.exec( diff );
-	console.log( changelogFilePath );
+console.log( rxString );
+
+if ( ! rx.test( diff ) ) {
+	console.log( 'no changelog detected' );
+	process.exit( 1 );
 }
+
+const [ changelogFilePath ] = rx.exec( diff );
+console.log( changelogFilePath );
